4a2e7d975c23eb01b5266e798cbd9e09df814817
Dan Pasanen <dan.pasanen@gmail.com>
2014-09-07 02:01:23 -0500

    envsetup: support specifying a different toolchain for kernel
    * set TARGET_KERNEL_TOOLCHAIN in your BoardConfig
    * example: TARGET_KERNEL_TOOLCHAIN := arm-eabi-4.9
    
    Change-Id: I1f17796a3a3e633a61f2556374856e8055a49031

--- envsetup.sh
+++ envsetup.sh
@@ -179,7 +179,11 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-eabi-$targetgccversion/bin
+            if [ -z $(get_build_var TARGET_KERNEL_TOOLCHAIN) ]; then
+                toolchaindir=arm/arm-eabi-$targetgccversion/bin
+            else
+                toolchaindir=arm/$(get_build_var TARGET_KERNEL_TOOLCHAIN)/bin
+            fi
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
