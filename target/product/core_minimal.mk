8c2b2374a9933464962d3d23ef3305e7d491c1f4
Adnan Begovic <adnan@cyngn.com>
2015-04-27 10:48:47 -0700

    build/target: Include CM platform jar in system server.
      Also lock to non-release type builds.
    
    Change-Id: Iaeecc4b1d8722f543b36cab76081cd49130a9eea

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -115,6 +115,12 @@ PRODUCT_SYSTEM_SERVER_APPS += \
     SettingsProvider \
     WallpaperBackup
 
+ifeq ($(filter NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
+# External system server capable jar
+PRODUCT_SYSTEM_SERVER_JARS += \
+    org.cyanogenmod.platform
+endif
+
 # Adoptable external storage supports both ext4 and f2fs
 PRODUCT_PACKAGES += \
     e2fsck \
