234b9dcec93900b74c16f8e3c98b4f2caf6b11fc
Adnan Begovic <adnan@cyngn.com>
2015-05-13 00:52:31 -0700

    build: Add IGNORE_SUBDIR to clear vars.
    Change-Id: Icc539d6d4e0a2d5a025416841dc05fe6bcb6199f

--- core/clear_vars.mk
+++ core/clear_vars.mk
@@ -427,6 +427,8 @@ LOCAL_CUSTOM_BUILD_STEP_INPUT:=
 LOCAL_CUSTOM_BUILD_STEP_OUTPUT:=
 LOCAL_IS_AUX_MODULE :=
 
+LOCAL_IGNORE_SUBDIR:=
+
 # Trim MAKEFILE_LIST so that $(call my-dir) doesn't need to
 # iterate over thousands of entries every time.
 # Leave the current makefile to make sure we don't break anything
