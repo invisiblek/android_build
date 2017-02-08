f7a55129c72a4d5ebd73b60ec7eab6ce0a2d24e4
Abhisek Devkota <ciwrl@lineageos.org>
2017-02-08 12:06:13 -0800

    Add dumpvar for WITH_SU
     -Allow folks to see easily if the term session they are in has
       WITH_SU set to true
     -Also add a dumpvar rule for WITH_GMS for those that have it
     -Remove unused CYNGN dumpvar rules
    
    Change-Id: Ib310d0016bb9eb16960f794720321b706b6fd8b0

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -27,6 +27,11 @@ print_build_config_vars := \
   OUT_DIR \
   AUX_OS_VARIANT_LIST
 
+ifeq ($(WITH_SU),true)
+print_build_config_vars += \
+  WITH_SU
+endif
+
 ifeq ($(TARGET_BUILD_PDK),true)
 print_build_config_vars += \
   TARGET_BUILD_PDK \
