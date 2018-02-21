d7c085d95d6b57304e9f57b669bd30909330fe24
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-21 14:21:49 -0600

    build: remove vendorsetup.sh
    Change-Id: I399a801617b6f18d2abcbe7e20237f264891bd0e

--- envsetup.sh
+++ envsetup.sh
@@ -1735,16 +1735,6 @@ if ! __detect_shell > /dev/null; then
     echo "WARNING: Only bash and zsh are supported, use of other shell may lead to erroneous results"
 fi
 
-# Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
-do
-    echo "including $f"
-    . $f
-done
-unset f
-
 addcompletions
 
 export ANDROID_BUILD_TOP=$(gettop)
